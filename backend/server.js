caa9afeb17faf804482ccccd6089bd79edb67ecd
Harsh <gupta.harshit0699@gmail.com>
2025-09-10 19:40:52 +0530

    made the database.js,  also established the connection with the database using connection  string and made server.js for testing that database is connected amd server is running or not

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const {connectDB} = require('./config/database');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Blogging API running...');
+});
+
+connectDB()
+  .then(() => {
+    console.log("Database Connection Established Successfully");
+
+    app.listen(4000, () => {
+      console.log("Server is Successfully listening on PORT : 4000");
+    });
+  })
+  .catch(() => {
+    console.log("Cannot Connect with database!!");
+  });
+
